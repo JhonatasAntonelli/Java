5b771220848b2cb904806f5810153f54231d5d15
Jhonatas Antonelli <jonatasantonelli@gmail.com>
2022-03-20 19:10:30 -0300

    ok

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package primeiroprograma;
+//apenas letras minusculas é nome do pacote
+//primeira letra maiuscula é classe ou interface
+//Todas as letras maiusculas nome de uma constante 
+//A primeira letra é minuscula e outras maiuscula, pode ser: atribulto, variável ou método 
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {// psvm é o atalho utilizado nesse caso
+        System.out.println("Olá, Mundo!");//sout é o atalho utilizado nesse caso e ln pula linha
+
+    }
+    
+}
+   
